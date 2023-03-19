79bfe5b387e710de2035b9b7fdcea0f3fadf2d10
prathamesh <prathamesh@webxstudio.in>
2023-03-19 13:27:11 +0530

    aboutus section added

--- frontend/components/AboutUs.js
+++ frontend/components/AboutUs.js
@@ -6,34 +6,107 @@ import icon3 from "../assets/AboutUs/icon3.png"
 
 const AboutUs = () => {
   return (
-    <div className="relative h-[43rem] flex flex-col-reverse md:grid md:grid-cols-2">
-        <div className="w-auto h-[10rem] md:h-auto ">
-            <Image src={splash} width={750} alt="" className="h-[10rem] md:h-[43rem] absolute z-0 bg-cover object-cover" />
-            <div className='bg-blue-900 h-[10rem] z-10 absolute  w-[100%] opacity-80 md:h-[43rem]  md:w-auto'></div>
-            <div className="absolute z-10 h-[10rem] grid grid-cols-3 gap-2 md:h-auto md:w-auto md:grid-rows-3 md:grid-cols-1 md:gap-8 ">
-                <div className="flex justify-center items-center flex-col">
-                    <Image src={icon3} alt="" className=" w-16" />
-                    <h1 className="text-base text-center">EPC</h1>
-                </div>
-
-                <div className="flex justify-center items-center flex-col">
-                    <Image src={icon1} alt="" className=" w-16" />
-                    <h1 className="text-base text-center">Import & Export Services</h1>
-                </div>
-
-                <div className="flex justify-center items-center flex-col">
-                    <Image src={icon2} alt="" className=" w-16" />
-                    <h1 className="text-base text-center">International Joint Venture projects</h1>
-                </div>
+    <div className="relative h-[43rem] grid grid-rows-3 md:grid-cols-3 md:grid-rows-1">
+      <div className="h-[15rem] w-full md:h-full">
+        <div className="relative z-10 flex justify-center">
+          <div className="absolute grid grid-cols-3 gap-12 my-10 md:gap-32 md:grid-rows-3 md:grid-cols-1">
+            <div className="flex justify-center items-center flex-col">
+              <Image src={icon3} alt="" />
+              <h2 className="text-center mt-3">EPC</h2>
             </div>
+
+            <div className="flex justify-center items-center flex-col">
+              <Image src={icon1} alt="" />
+              <h2 className="text-center mt-3">Import & Export Services</h2>
+            </div>
+
+            <div className="flex justify-center items-center flex-col">
+              <Image src={icon2} alt="" />
+              <h2 className="text-center mt-3">
+                International Joint Venture projects
+              </h2>
+            </div>
+          </div>
         </div>
-        <div className="h-[33rem]">
-            <h1 className="text-green-500">
-                Aboutus
+        <div className="relative h-full w-full md:h-full bg-black opacity-90 ">
+          <Image
+            className=" object-cover object-top brightness-50"
+            src={splash}
+            fill
+            alt=""
+          />
+        </div>
+      </div>
+      <div className="row-span-2 md:col-span-2 px-6 md:px-8 relative md:w-5/6">
+        <div className="flex justify-start items-center mt-16">
+          <div className=" border-x-[2rem] border-y-[0.1rem] border-green-500">
+            <h1 className="hidden">-</h1>
+          </div>
+          <h2 className="text-green-500 ml-4 text-xl font-bold">About us</h2>
+        </div>
+        <div className="mt-8 ml-2 flex justify-start items-center">
+          <div>
+            <div className="grid grid-cols-3 w-16 ">
+              <div className=" w-3 h-3 rounded-full bg-black"></div>
+              <div className=" w-3 h-3 rounded-full bg-green-500"></div>
+              <div className=" w-3 h-3 rounded-full bg-black"></div>
+            </div>
+
+            <div className="grid grid-cols-3 w-16  mt-2">
+              <div className=" w-3 h-3 rounded-full bg-white"></div>
+              <div className=" w-3 h-3 rounded-full bg-black"></div>
+              <div className=" w-3 h-3 rounded-full bg-black"></div>
+            </div>
+
+            <div className="grid grid-cols-3 w-16  mt-2">
+              <div className=" w-3 h-3 rounded-full bg-white"></div>
+              <div className=" w-3 h-3 rounded-full bg-white"></div>
+              <div className=" w-3 h-3 rounded-full bg-green-500"></div>
+            </div>
+          </div>
+          <div className="ml-4">
+            <h1 className="text-black">
+              Welcome To Airosea International Ltd.
             </h1>
+          </div>
         </div>
+        <div className="my-6 mx-2">
+          <h2 className="text-black text-left font-semibold">
+            Air O Sea International Ltd. is a disciplined multinational, dynamic
+            organization with High Value and Integrity. We are strong bridge
+            between India and China Businesses. With strong experience in this
+            area we are providing values to our customers in various service
+            lines. We not only introduce the business opportunities but making
+            our customers comfortable in overseas business.
+          </h2>
+        </div>
+
+        <div className="mx-2">
+          <h2 className="text-black text-left font-semibold">
+            Our core services includes and not limited to Trading , Import –
+            Export, End to End Project Management, Business Consulting. We are
+            having Registered Office in Hong Kong and Corporate office in Pune,
+            India and operating from China. We have strong network of partners
+            which gives us the knowledge and capacity to give you ideal and best
+            in the industry output. We have in-depth knowledge and strong
+            experience in a wide range of products such as Electronics,
+            Furnitures, LEDs, Gifts, Textiles, Tools , Toys, Food, Appliances,
+            Mobile and many more.
+          </h2>
+        </div>
+
+        <div className="mx-2">
+          <h2 className="text-black text-left font-semibold">
+            At Air O Sea International Ltd., we give you Risk free access to
+            China’s abundant sourcing opportunities. We are focused on Services
+            and have the passion to move in all diversified area for our
+            customers. It’s our commitment to assist our customers in their
+            organization’s growth.
+          </h2>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default AboutUs
--- frontend/components/Caraousal.js
+++ frontend/components/Caraousal.js
@@ -8,46 +8,51 @@ import Navbar from '../components/Navbar'
 
 const Caraousal = () => {
   return (
-    <div className="relative h-[40rem] md:h-[50rem] ">
-        <div className='bg-black h-[40rem] z-10 absolute  w-[100%] md:h-[50rem] opacity-80'></div>
-        <Image
-          src={splash}
-          alt=""
-          className="hidden z-0 absolute w-[full] h-[50rem] opacity-70 object-cover md:flex"
-        />
-        <Image
-          src={splash2}
-          alt=""
-          className=" z-0 absolute w-[full] h-[40rem] opacity-70 object-cover md:hidden"
-        />
-        <div className="m-4 z-20   absolute md:grid md:grid-cols-2 md:gap-44 md:px-36">
-          <div className="">
-            <Image src={logo} alt="" className="w-[20rem]  md:w-[30rem]" />
-          </div>
-          <div className="hidden md:flex justify-center">
-            <Navbar />
-          </div>
+    <div className="relative h-[40rem] md:h-[50rem] bg-red-900 overflow-hidden">
+      <div className="bg-black h-[40rem] z-10 absolute  w-[100%] md:h-[50rem] opacity-80"></div>
+      <Image
+        src={splash}
+        alt=""
+        className="hidden z-0 absolute w-[full] h-[50rem] opacity-70 object-cover md:flex"
+      />
+      <Image
+        src={splash2}
+        alt=""
+        className=" z-0 absolute w-[full] h-[40rem] opacity-70 object-cover md:hidden"
+      />
+      <div className="m-4 z-20 w-full  absolute md:flex">
+        <div className="md:ml-10">
+          <Image
+            src={logo}
+            alt=""
+            className="min-w-[5rem] w-[20rem]  md:w-[25rem] md:min-w-[10rem]"
+          />
         </div>
-        <div className="flex z-20 flex-col m-4 mt-[10rem] md:mt-[15rem] absolute md:px-36 md:grid md:grid-cols-5 ">
-          <div className='md:ml-12 md:col-span-3'>
-            <h1 className="text-white text-2xl font-bold  md:text-[2.5rem] md:leading-[3rem]">
-              Are you looking to import or shop contemporary furniture from china
-              to India?
-            </h1>
-            <h2 className=' mt-8 text-base md:text-md'>
-              We can help you at each and every step of your logistic
-              requirements. just fill up the below form and our experts will get
-              in touch with you and help you on getting your products at your door
-              step.
-            </h2>
-            <div className='bg-blue-900 px-6 py-2 absolute mt-8 cursor-pointer  rounded-md'>
-                <h2 className='text-white text-md'>Contact Us</h2>
-            </div>
-          </div>
+        <div className="hidden md:hidden lg:w-3/6 lg:flex lg:justify-end xl:w-4/6">
+          <Navbar />
         </div>
-        
       </div>
-  )
+      <div className="flex z-20 flex-col m-4 mt-[10rem] sm:w-5/6 md:mt-[12rem] absolute  md:grid md:grid-cols-5 lg:w-5/6">
+        <div className="md:ml-12 md:col-span-3">
+          <h1 className="text-white text-2xl font-bold  md:text-[2.5rem] md:leading-[3rem]">
+            Are you looking to import or shop contemporary furniture from china
+            to India?
+          </h1>
+          <h2 className=" mt-8 text-base md:text-lg">
+            We can help you at each and every step of your logistic
+            requirements.
+          </h2>
+          <h2 className="text-base md:text-lg">
+            just fill up the below form and our experts will get in touch with
+            you and help you on getting your products at your door step.
+          </h2>
+          <div className="bg-blue-900 px-6 py-2 absolute mt-8 cursor-pointer  rounded-md">
+            <h2 className="text-white text-md">Contact Us</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Caraousal
